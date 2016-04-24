1c5e5ec4e3611980333bfff8543c95adfa516c71
gyuan <gyuan@walmartlabs.com>
2016-04-23 18:51:30 -0700

    modified:   bash.sh 	new file:   envrc

--- bash.sh
+++ bash.sh
@@ -1,4 +1,17 @@
 #/bin/bash
-set -ex
+set -e
+source envrc
 
+epoch_start=`get_epoch`
+echo "Start @$epoch_start"
+#########################
+
+echo "$TODAY"
+echo "$NOW"
+echo "$RUN_MODE"
+sleep 1s
 echo 'Done'
+
+#########################
+epoch_end=`get_epoch`
+echo "End @$epoch_end"
